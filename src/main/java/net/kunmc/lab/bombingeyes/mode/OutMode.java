c0798cec088a70b59dafabe72a2ba764d71737ad
BosatsuNumNum <bosatsunumnum@gmail.com>
2021-01-30 22:28:52 +0900

    例外の発生を回避 リストの挙動を修正

--- src/main/java/net/kunmc/lab/bombingeyes/mode/OutMode.java
+++ src/main/java/net/kunmc/lab/bombingeyes/mode/OutMode.java
@@ -1,30 +1,20 @@
 package net.kunmc.lab.bombingeyes.mode;
 
-import com.destroystokyo.paper.event.server.ServerExceptionEvent;
 import net.kunmc.lab.bombingeyes.BombingEyes;
 import net.kunmc.lab.bombingeyes.Config;
 import net.kunmc.lab.bombingeyes.Const;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
-import org.bukkit.block.Block;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 
 public class OutMode extends CommonProcess {
 
     private static OutMode instance;
-    private static List<OutModePlayer> playerList = new ArrayList<>();
-
-    private OutMode() {
-    }
+    List<OutModePlayer> playerList = new ArrayList<>();
 
     public static OutMode getInstance() {
         if (instance == null) {
@@ -40,36 +30,40 @@ public class OutMode extends CommonProcess {
         BukkitRunnable process = new BukkitRunnable() {
             @Override
             public void run() {
-            // プレイヤーのタイマーを確認
-            for (OutModePlayer p : playerList) {
-                if (p.getTime() > Config.timeLimit) {
-                    // プレイヤーの爆破処理
-                    OutMode.super.bombing(p.getPlayer());
-                    // リストから除外
-                    playerList.remove(p);
-                }
-            }
-
-            // 視界内判定
-            for (Player killer : ModeController.killerList) {
-                for (OutModePlayer player : playerList) {
-                    if (isInSight(killer, player.getPlayer())) {
-                        // 視界内に入ったプレイヤーのタイマーをリセット
-                        player.clearTime();
-                    } else {
-                        // 視界外のプレイヤーのタイマーを進める
-                        player.incrementTime();
+                try {
+                    // プレイヤーのタイマーを確認
+                    for (OutModePlayer player : playerList) {
+                        if (player.getTime() >= Config.timeLimit) {
+                            // プレイヤーの爆破処理
+                            OutMode.super.bombing(player.getPlayer());
+                            // リストから除外
+                            playerList.remove(player);
+                        }
                     }
-                }
-            }
 
-            // モードが切り替わったとき
-            if (!ModeController.runningMode.equals(Const.MODE_BE_IN)) {
-                // プロセス終了
-                cancel();
-                playerList.clear();
-                return;
-            }
+                    // 視界内判定
+                    for (Player killer : ModeController.killerList) {
+                        for (OutModePlayer player : playerList) {
+                            if (isInSight(killer, player.getPlayer())) {
+                                // 視界内に入ったプレイヤーのタイマーをリセット
+                                player.clearTime();
+                            } else {
+                                // 視界外のプレイヤーのタイマーを進める
+                                player.incrementTime();
+                            }
+                        }
+                    }
+
+                    // モードが切り替わったとき
+                    if (!ModeController.runningMode.equals(Const.MODE_BE_OUT)) {
+                        // プロセス終了
+                        cancel();
+                        playerList.clear();
+                        return;
+                    }
+                } catch (Exception e) {
+                }
+
             }
         };
         process.runTaskTimer(BombingEyes.getPlugin(), 0L, Config.tick);
@@ -83,7 +77,7 @@ public class OutMode extends CommonProcess {
         for (Player player : Bukkit.getOnlinePlayers()) {
             // ゲームモードがクリエイティブorスぺクテイターの場合は追加しない
             if (player.getGameMode().equals(GameMode.CREATIVE) || player.getGameMode().equals(GameMode.SPECTATOR)) {
-                return;
+                continue;
             }
             if (!ModeController.killerList.contains(player)) {
                 playerList.add(new OutModePlayer(player));
@@ -97,13 +91,8 @@ public class OutMode extends CommonProcess {
      * @param player
      */
     void addPlayer(Player player) {
-        for (OutModePlayer p : playerList) {
-            if (p.getPlayer().equals(player)) {
-                playerList.remove(p);
-            }
-            if (!ModeController.killerList.contains(player)) {
-                playerList.add(new OutModePlayer(player));
-            }
+        if (!ModeController.killerList.contains(player)) {
+            playerList.add(new OutModePlayer(player));
         }
     }
 
